7083dd5ac4c15e1bf93af13c0179ab39223b0f6e
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2020-05-22 09:15:21 +0300

    add udev rules for ergodox firmware uploading

--- configuration.nix
+++ configuration.nix
@@ -408,6 +408,21 @@ in
       };
     };
 
+    udev = {
+      extraRules = ''
+        # Teensy rules for the Ergodox EZ Original / Shine / Glow
+        ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+        ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+        KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+
+        # STM32 rules for the Planck EZ Standard / Glow
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
+            MODE:="0666", \
+            SYMLINK+="stm32_dfu"
+      '';
+    };
+
     printing.enable = false; # CUPS to print documents (have no printer yet)
 
     # X11
