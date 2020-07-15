0b339b1c7b26e9ab04bba4c827e5aa958871e3c1
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2020-07-15 21:10:49 +0300

    use "psi-plus" of version 1.4.1463

--- configuration.nix
+++ configuration.nix
@@ -35,7 +35,17 @@ let
     # Whilst in unstable nixpkgs it's 2020.05.
     rakudo = unstable-nixpkgs.pkgs.rakudo;
 
-    psi-plus = unstable-nixpkgs.pkgs.psi-plus; # Just newer snapshot version
+    # Latest on July 15, 2020
+    psi-plus = unstable-nixpkgs.pkgs.psi-plus.overrideAttrs (srcAttrs: srcAttrs // rec {
+      version = "1.4.1463";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "psi-plus";
+        repo = "psi-plus-snapshots";
+        rev = version;
+        sha256 = "0x1ck2rdk51n99jzv291qg745y8f42prpm11xgn9m3li0hn47di7";
+      };
+    });
   };
 
   inherit (utils) esc wrapExecutable;
