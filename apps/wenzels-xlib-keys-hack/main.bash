28296217856c6598f4822bbad8f12c314928475f
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2020-08-23 01:49:15 +0300

    wenzels-xlib-keys-hack: Some refactoring
    - Parse arguments
    - Add more comments
    - Fix typos

--- apps/wenzels-xlib-keys-hack/main.bash
+++ apps/wenzels-xlib-keys-hack/main.bash
@@ -5,25 +5,18 @@ printf -- '->> STARTING at %s … <<-\n' "$(date)"
 
 grant-access-to-input-devices
 
+# Feature turning on/off flags (arguments list for “xlib-keys-hack”)
 FLAGS=(
 	--disable-reset-by-window-focus-event
 )
 
-# IPC stuff
+# Turning on IPC stuff
 FLAGS+=(
 	--xmobar-indicators
 	--external-control
 )
 
-DUCKY=(
-	'--disable-xinput-device-name=Ducky Ducky One2 Mini RGB'
-	'/dev/input/by-id/usb-Ducky_Ducky_One2_Mini_RGB_DK-V1.08-190201-event-if03'
-	'/dev/input/by-id/usb-Ducky_Ducky_One2_Mini_RGB_DK-V1.08-190201-event-kbd'
-	'/dev/input/by-id/usb-Ducky_Ducky_One2_Mini_RGB_DK-V1.08-190201-if01-event-mouse'
-	'/dev/input/by-id/usb-Ducky_Ducky_One2_Mini_RGB_DK-V1.08-190201-if01-mouse'
-	'/dev/input/by-id/usb-Ducky_Ducky_One2_Mini_RGB_DK-V1.08-190201-if02-event-kbd'
-)
-
+# Corsair K63 mechanical gaming keyboard
 CORSAIR=(
 	'--disable-xinput-device-name=Corsair Corsair Gaming K63 Keyboard'
 	'--disable-xinput-device-name=Corsair Corsair Gaming K63 Keyboard Keyboard'
@@ -33,11 +26,23 @@ CORSAIR=(
 	'/dev/input/by-id/usb-Corsair_Corsair_Gaming_K63_Keyboard_0B008012AF0C1D2558ED9875F5001945-if01-event-kbd'
 )
 
+# Embedded laptop keyboard
 EMBEDDED=(
 	'--disable-xinput-device-name=AT Translated Set 2 keyboard'
 	# See below for devices list
 )
 
+# Ducky One 2 Mini RGB keyboard
+DUCKY=(
+	'--disable-xinput-device-name=Ducky Ducky One2 Mini RGB'
+	'/dev/input/by-id/usb-Ducky_Ducky_One2_Mini_RGB_DK-V1.08-190201-event-if03'
+	'/dev/input/by-id/usb-Ducky_Ducky_One2_Mini_RGB_DK-V1.08-190201-event-kbd'
+	'/dev/input/by-id/usb-Ducky_Ducky_One2_Mini_RGB_DK-V1.08-190201-if01-event-mouse'
+	'/dev/input/by-id/usb-Ducky_Ducky_One2_Mini_RGB_DK-V1.08-190201-if01-mouse'
+	'/dev/input/by-id/usb-Ducky_Ducky_One2_Mini_RGB_DK-V1.08-190201-if02-event-kbd'
+)
+
+# Boolean flag indicates that Ducky is connected
 HAS_DUCKY=0
 for x in "${DUCKY[@]}"; do
 	if [[ $x =~ ^/dev/input/ && -r $x ]]; then
@@ -46,6 +51,7 @@ for x in "${DUCKY[@]}"; do
 	fi
 done
 
+# ZSA ErgoDox EZ keyboard
 ERGODOX_EZ=(
 	# old firmware
 	'--disable-xinput-device-name=ErgoDox EZ ErgoDox EZ'
@@ -56,6 +62,7 @@ ERGODOX_EZ=(
 	/dev/input/by-id/usb-ZSA_Ergodox_EZ_0-event-kbd
 )
 
+# Boolean flag indicates that Ergodox is connected
 HAS_ERGODOX_EZ=0
 for x in "${ERGODOX_EZ[@]}"; do
 	if [[ $x =~ ^/dev/input/ && -r $x ]]; then
@@ -64,30 +71,53 @@ for x in "${ERGODOX_EZ[@]}"; do
 	fi
 done
 
-PLANK_EZ=(
+# ZSA Planck EZ keyboard
+PLANCK_EZ=(
 	'--disable-xinput-device-name=ZSA Planck EZ'
 	/dev/input/by-id/usb-ZSA_Planck_EZ_0-event-kbd
 )
 
-HAS_PLANK_EZ=0
-for x in "${PLANK_EZ[@]}"; do
+# Boolean flag indicates that Planck is connected
+HAS_PLANCK_EZ=0
+for x in "${PLANCK_EZ[@]}"; do
 	if [[ $x =~ ^/dev/input/ && -r $x ]]; then
-		HAS_PLANK_EZ=1
+		HAS_PLANCK_EZ=1
 		break
 	fi
 done
 
-MODE=${1:-}
+# A list of available modes (for arguments validation)
+declare -A MODES && MODES=([gaming]=1 [no-numbers-shift]=1)
 
+# Parse command-line arguments
+if (( $# == 0 )); then
+	MODE=
+elif (( $# == 1 )) && [[ -n ${MODES[$1]:-} ]]; then
+	MODE=$1
+else
+	>&2 echo 'Arguments are incorrect:'
+	>&2 printf ' "%s"' "${@//\"}"
+	>&2 echo
+	exit 1
+fi
+
+# When gaming mode is on “additional controls” feature should be disabled so those keys will be
+# triggered immediately when they are just pressed.
+# Also on a regular qwerty keyboard it’s more useful to have Caps Lock key instead of remapped
+# Escape on its place so you can still have Escape for whatever purpuse in a game. And in some games
+# you can’t even map Escape as you would map any other key since it’s reserved to call game menu for
+# instance.
 if [[ $MODE == gaming ]]; then
 	echo '[ GAMING MODE ON ]'
-	if (( HAS_ERGODOX_EZ == 0 && HAS_PLANK_EZ == 0 )); then
+	if (( HAS_ERGODOX_EZ == 0 && HAS_PLANCK_EZ == 0 )); then
 		FLAGS+=(
 			--real-capslock
 			--no-additional-controls
 		)
 	fi
-elif (( HAS_ERGODOX_EZ == 0 && HAS_PLANK_EZ == 0 )); then
+
+# Ergonomic mode for regular qwerty keyboard
+elif (( HAS_ERGODOX_EZ == 0 && HAS_PLANCK_EZ == 0 )); then
 	FLAGS+=(
 		--hold-alt-for-alternative-mode
 		--ergonomic-mode
@@ -95,7 +125,7 @@ elif (( HAS_ERGODOX_EZ == 0 && HAS_PLANK_EZ == 0 )); then
 fi
 
 # Turn off embedded keyboard if there's external one
-if (( HAS_DUCKY == 0 && HAS_ERGODOX_EZ == 0 && HAS_PLANK_EZ == 0 )); then
+if (( HAS_DUCKY == 0 && HAS_ERGODOX_EZ == 0 && HAS_PLANCK_EZ == 0 )); then
 	EMBEDDED+=(
 		'/dev/input/by-path/platform-i8042-serio-0-event-kbd'
 		'/dev/input/by-path/platform-i8042-serio-1-event-mouse'
@@ -103,7 +133,12 @@ if (( HAS_DUCKY == 0 && HAS_ERGODOX_EZ == 0 && HAS_PLANK_EZ == 0 )); then
 	)
 fi
 
-if (( HAS_ERGODOX_EZ == 0 && HAS_PLANK_EZ == 0 )); then
+# If it’t a regular qwerty keyboard (including Ducky).
+# Shift “hjkl” on software level.
+# Turn on software debouncer (in order to fix issues with key bouncing on Ducky).
+# Right Control as Super to have simmetrical Supers on my laptop’s embedded keyboard
+# (control keys are provided by “additional controls” feature anyway).
+if (( HAS_ERGODOX_EZ == 0 && HAS_PLANCK_EZ == 0 )); then
 	FLAGS+=(
 		--shift-hjkl
 		'--software-debouncer=90'
@@ -113,6 +148,8 @@ if (( HAS_ERGODOX_EZ == 0 && HAS_PLANK_EZ == 0 )); then
 	if [[ $MODE == no-numbers-shift ]]; then
 		FLAGS+=( --shift-numeric-keys )
 	fi
+
+# Either ErgoDox or Planck
 else
 	FLAGS+=(
 		--real-capslock
@@ -120,15 +157,18 @@ else
 		--f24-as-vertical-bar
 		--reset-by-real-escape
 	)
-	# Keep additional controls feature for Plank EZ
-	if (( HAS_PLANK_EZ == 0 )); then
+	# Keep additional controls feature for Planck EZ
+	if (( HAS_PLANCK_EZ == 0 )); then
 		FLAGS+=( --no-additional-controls )
 	fi
+	# Super double press feature may stand in your way if you press Super key too often in a game
 	if [[ $MODE == gaming ]]; then
 		FLAGS+=(
 			--disable-super-double-press
 			# --right-super-as-space
 		)
+	# Alternative mode turned on by held Alt keys is probably not a good idea in gaming mode.
+	# In gaming mode your probably want to trigger Alts immediately when you press them.
 	else
 		FLAGS+=(
 			--hold-alt-for-alternative-mode
@@ -136,13 +176,14 @@ else
 	fi
 fi
 
+# All arguments combined for “xlib-keys-hack”
 ALL_XKH_ARGS=(
 	"${FLAGS[@]}"
 	"${EMBEDDED[@]}"
 	"${DUCKY[@]}"
 	"${CORSAIR[@]}"
 	"${ERGODOX_EZ[@]}"
-	"${PLANK_EZ[@]}"
+	"${PLANCK_EZ[@]}"
 )
 
 xlib-keys-hack "${ALL_XKH_ARGS[@]}" &
