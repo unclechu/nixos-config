ec2cb7314b2b19ac32e0fc98d486c8107b3c9e4e
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2020-09-21 02:48:47 +0300

    Fix OpenGL 32bit config

--- configuration.nix
+++ configuration.nix
@@ -196,20 +196,23 @@ in
       package = pkgs.pulseaudio.override { jackaudioSupport = true; };
     };
 
-    opengl = rec {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-
-      extraPackages = [
-        pkgs.mesa
-        pkgs.vaapiVdpau
-        pkgs.vaapiIntel
-        pkgs.libvdpau-va-gl
-      ];
-
-      extraPackages32 = extraPackages;
-    };
+    opengl =
+      let
+        extras = [
+          "mesa"
+          "vaapiVdpau"
+          "vaapiIntel"
+          "libvdpau-va-gl"
+          "libva"
+        ];
+      in
+        {
+          enable = true;
+          driSupport = true;
+          driSupport32Bit = true;
+          extraPackages = builtins.map (name: pkgs.${name}) extras;
+          extraPackages32 = builtins.map (name: pkgs.pkgsi686Linux.${name}) extras;
+        };
   };
 
   services = {
