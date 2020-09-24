4f3bf5540dc8384c9431f77e8bd4bcb8c579b861
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2020-09-24 05:57:45 +0300

    Override neovim-qt to latest version

--- pkgs.nix
+++ pkgs.nix
@@ -26,4 +26,43 @@ stable-pkgs // {
       "-DCHAT_TYPE=BASIC"
     ];
   });
+
+  neovim-qt =
+    let
+      unwrapped =
+        stable-pkgs.neovim-qt.unwrapped.overrideAttrs (a: a // rec {
+          version = "0.2.16.1";
+
+          src = fetchFromGitHub {
+            owner  = "equalsraf";
+            repo   = "neovim-qt";
+            rev    = "v${version}";
+            sha256 = "0x5brrim3f21bzdmh6wyrhrislwpx1248wbx56csvic6v78hzqny";
+          };
+
+          cmakeFlags = [
+            "-DUSE_SYSTEM_MSGPACK=1"
+            "-DENABLE_TESTS=0"
+          ];
+        });
+    in
+      stable-pkgs.makeOverridable ({ neovim }: stable-pkgs.neovim-qt.overrideAttrs (a: a // {
+        version = unwrapped.version;
+
+        buildCommand = ''
+          makeWrapper '${unwrapped}/bin/nvim-qt' "$out/bin/nvim-qt" \
+            --prefix PATH : "${neovim}/bin"
+
+          # link .desktop file
+          mkdir -p "$out/share/pixmaps"
+          ln -s '${unwrapped}/share/applications' "$out/share/applications"
+          ln -s '${unwrapped}/share/pixmaps/nvim-qt.png' "$out/share/pixmaps/nvim-qt.png"
+        '';
+
+        passthru = {
+          inherit unwrapped;
+        };
+
+        inherit (unwrapped) meta;
+      })) { inherit (stable-pkgs) neovim; };
 }
