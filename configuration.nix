4c1ecbe71407bc0ac57d217723f7f2a72ede4d99
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2020-09-27 22:29:08 +0300

    Separate general “boot” configuration into own module

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-args@{ config, pkgs, ... }:
+args@{ config, pkgs, options, ... }:
 let
   config-k = "config";
 
@@ -33,44 +33,13 @@ in
   imports = [
     (import "${home-manager}/nixos")
     my-packages.configuration
+    ./boot.nix
     ./machine-specific.nix
   ];
 
   system = {
     stateVersion = "20.09";
     fsPackages = [pkgs.xfsprogs.bin];
-  };
-
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-
-      grub = {
-        enable = true;
-        version = 2;
-        device = "nodev";
-        efiSupport = true;
-        enableCryptodisk = true;
-      };
-
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot/efi";
-      };
-    };
-
-    initrd = {
-      availableKernelModules = [
-        "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"
-        "xfs" "crc32c"
-        "nvme"
-      ];
-
-      # kernelModules = [];
-    };
-
-    # kernelModules = [];
-    # extraModulePackages = [];
   };
 
   networking = {
