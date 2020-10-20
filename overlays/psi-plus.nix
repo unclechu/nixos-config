273b9fd58bc5f1d4f812bd196e4afcc1f0f8529d
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2020-10-20 20:03:29 +0300

    Update “psi-plus”

--- overlays/psi-plus.nix
+++ overlays/psi-plus.nix
@@ -1,13 +1,13 @@
 self: super:
 {
   psi-plus = super.psi-plus.overrideAttrs (srcAttrs: srcAttrs // rec {
-    version = "1.4.1515"; # Latest on October 10, 2020
+    version = "1.4.1520"; # Latest on October 18, 2020
 
     src = super.fetchFromGitHub {
       owner = "psi-plus";
       repo = "psi-plus-snapshots";
       rev = version;
-      sha256 = "0kl9iy5ycbvbsi7izbi45wg7zss675k28ss80zvzr9pagdnc0jvr";
+      sha256 = "17ih8c1k4wabrp172p4hsywil8ad5c53r4jh9zc1p7f499083srl";
     };
 
     cmakeFlags = [
