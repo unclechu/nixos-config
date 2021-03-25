8effea68dca7b9dc967045fab1170dc982a2e89c
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-03-25 21:42:14 +0200

    Refactor dependencies management in ‘apps/wenzels-xbindkeys.nix’

--- apps/wenzels-xbindkeys.nix
+++ apps/wenzels-xbindkeys.nix
@@ -2,6 +2,7 @@ let sources = import ../nix/sources.nix; in
 { callPackage
 , runCommand
 , writeText
+, coreutils
 # , xkb-switch
 , xautolock
 , xbindkeys
@@ -13,22 +14,34 @@ let
   inherit (__nix-utils) nameOfModuleFile wrapExecutable esc shellCheckers;
   name = nameOfModuleFile (builtins.unsafeGetAttrPos "a" { a = 0; }).file;
 
+  # Name is executable name and value is a derivation that provides that executable
+  dependencies = {
+    mkdir = coreutils;
+    cp = coreutils;
+    chmod = coreutils;
+    # xkb-switch = xkb-switch;
+    xautolock = xautolock;
+    xbindkeys = xbindkeys;
+    xbindkeys_show = xbindkeys;
+  };
+
+  executables = builtins.mapAttrs (n: v: "${dependencies.${n}}/bin/${n}") dependencies;
+
   # previously i was confused with some weird behavior like layouts were rotating not sequentially.
   # but i realized its LShift+RShift for rotating forward and RShift+LShift for backward rotation
   # which is a lot more convenient.
   #
   # old hooks for one-direction rotation:
   #
-  #   "${xkb-switch-exe} -n"
+  #   "${executables.xkb-switch} -n"
   #     shift + c:50
-  #   "${xkb-switch-exe} -n"
+  #   "${executables.xkb-switch} -n"
   #     shift + c:62
   #
-  # xkb-switch-exe = "${xkb-switch}/bin/xkb-switch";
-  # ${shellCheckers.fileIsExecutable xkb-switch-exe}
+  # ${shellCheckers.fileIsExecutable executables.xkb-switch}
 
   xbindkeysrc = writeText "wenezels-xbindkeysrc" ''
-    "${xautolock-exe} -locknow"
+    "${executables.xautolock} -locknow"
       XF86Eject
 
     # recursive dependency, can't use store path
@@ -36,25 +49,22 @@ let
       shift + XF86Eject
   '';
 
-  xautolock-exe = "${xautolock}/bin/xautolock";
-  xbindkeys-origin = "${xbindkeys}/bin/xbindkeys";
-  xbindkeys_show-origin = "${xbindkeys}/bin/xbindkeys_show";
-
   checkPhase = ''
-    ${shellCheckers.fileIsExecutable xautolock-exe}
-    ${shellCheckers.fileIsExecutable xbindkeys-origin}
-    ${shellCheckers.fileIsExecutable xbindkeys_show-origin}
+    ${
+      builtins.concatStringsSep "\n"
+        (map shellCheckers.fileIsExecutable (builtins.attrValues executables))
+    }
     ${shellCheckers.fileIsReadable xbindkeysrc}
   '';
 
   configArgs = [ "-f" xbindkeysrc ];
 
-  wenzels-xbindkeys = wrapExecutable xbindkeys-origin {
+  wenzels-xbindkeys = wrapExecutable executables.xbindkeys {
     inherit name checkPhase;
     args = configArgs;
   };
 
-  wenzels-xbindkeys_show = wrapExecutable xbindkeys_show-origin {
+  wenzels-xbindkeys_show = wrapExecutable executables.xbindkeys_show {
     inherit checkPhase;
     name = "${name}_show";
     args = configArgs;
@@ -62,14 +72,16 @@ let
 in
 runCommand name {} ''
   set -Eeuo pipefail || exit
-  mkdir -p -- "$out/bin"
+  ${esc executables.mkdir} -p -- "$out/bin"
 
-  cp -- \
+  ${esc executables.cp} -- \
     ${esc wenzels-xbindkeys}/bin/${esc wenzels-xbindkeys.name} \
     ${esc wenzels-xbindkeys_show}/bin/${esc wenzels-xbindkeys_show.name} \
     "$out"/bin/
 
-  chmod +x -- "$out"/bin/${esc wenzels-xbindkeys.name} "$out"/bin/${esc wenzels-xbindkeys_show.name}
+  ${esc executables.chmod} +x -- \
+    "$out"/bin/${esc wenzels-xbindkeys.name} \
+    "$out"/bin/${esc wenzels-xbindkeys_show.name}
 '' // {
   xbindkeys = wenzels-xbindkeys;
   xbindkeys_show = wenzels-xbindkeys_show;
