b97a8404e108427c6c8c68fedee2634ea6b39987
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-08-26 04:07:43 +0300

    Update “neovim-qt” overlay pin

--- overlays/neovim-qt.nix
+++ overlays/neovim-qt.nix
@@ -2,21 +2,18 @@
 # License: MIT https://raw.githubusercontent.com/unclechu/nixos-config/master/LICENSE
 self: super:
 {
+  # Just a fresher version from the “master” branch
   neovim-qt =
     let
       unwrapped =
         super.neovim-qt.unwrapped.overrideAttrs (srcAttrs: srcAttrs // rec {
-          # Testing ligatures support, it’s planned to be released with 0.2.17 later.
-          # See https://github.com/equalsraf/neovim-qt/issues/166#issuecomment-700994778
-          version = "master";
-          # version = "0.2.16.1";
+          version = "git-master";
 
           src = super.fetchFromGitHub {
             owner  = "equalsraf";
             repo   = "neovim-qt";
-            # rev    = "v${version}";
-            rev    = "5038ad11d27100889a69b079f1c0c2515687565a"; # September 30, 2020
-            sha256 = "0vcqrbbk6p1q6vlmqa2nsf3pn46r3wmf0rpv9l75pln7xav0qbn0";
+            rev    = "31c7f6e35d488a557209d1f81afd821bf3b73824"; # 25 August 2021
+            sha256 = "03wvra8i0x7pws1dpj207g4qq1nl5j8llz79vd0hqjs8pikpp4li";
           };
 
           cmakeFlags = [
