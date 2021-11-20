185e6676665ee730059151cec230e461029577be
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-11-21 00:57:41 +0200

    Update Ψ+ from 1.5.1557-git-master to 1.5.1582

--- apps/psi-plus.nix
+++ apps/psi-plus.nix
@@ -3,12 +3,12 @@
 { fetchFromGitHub, psi-plus }:
 
 (psi-plus.override { enablePsiMedia = true; }).overrideAttrs (_: {
-  version = "1.5.1557-git-master"; # The commit is later than the actual version (plugins update)
+  version = "1.5.1582";
 
   src = fetchFromGitHub {
     owner = "psi-plus";
     repo = "psi-plus-snapshots";
-    rev = "1e4b513d7f8bd6de7f567137bf97bfbaa6f1b1b1"; # 30 October 2021
-    sha256 = "1m45y32sslxab6g6syb23ywsw20jf70wxfn39yga5ddnafig2fpr";
+    rev = "92fe048134c24cc9f3dc21d83cca12587fc81dea"; # 15 November 2021
+    sha256 = "0vmccxrs8ga4s2xzj00xijk8azyihycg1fp5bfgqvf833hdp3hk4";
   };
 })
