8ca5e43e342f19444066cef70c88c4fb9250b0fa
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-11-23 01:37:02 +0200

    “amdgpu” overlay
    Use a newer one from <nixos-unstable>.
    I was trying to debug a problem, the driver update didn’t help anyway,
    but I would like to just keep the newer version.

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,7 @@
 # Author: Viacheslav Lotsmanov
 # License: MIT https://raw.githubusercontent.com/unclechu/nixos-config/master/LICENSE
 [
+  (import ./amdgpu.nix)
   (import ./neovim-qt.nix)
   (import ./dino.nix)
 ]
