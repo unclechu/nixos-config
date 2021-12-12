e5e0397c8d8e77727a98c0eeebf49e35544087b3
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-12-12 15:32:15 +0200

    Add WebP support for Psi+

--- apps/psi-plus.nix
+++ apps/psi-plus.nix
@@ -1,8 +1,8 @@
 # Author: Viacheslav Lotsmanov
 # License: MIT https://raw.githubusercontent.com/unclechu/nixos-config/master/LICENSE
-{ fetchFromGitHub, psi-plus }:
+{ fetchFromGitHub, psi-plus, libsForQt5 }:
 
-(psi-plus.override { enablePsiMedia = true; }).overrideAttrs (_: rec {
+(psi-plus.override { enablePsiMedia = true; }).overrideAttrs (srcAttrs: rec {
   version = "1.5.1588";
 
   src = fetchFromGitHub {
@@ -11,4 +11,8 @@
     rev = version;
     sha256 = "1qylmq4spay71c6q9xrm311vx54iji1ga4ij07k51598fg5dba8d";
   };
+
+  buildInputs = srcAttrs.buildInputs ++ [
+    libsForQt5.qtimageformats
+  ];
 })
