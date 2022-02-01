a8b44c5e3c8d541946e45aaf56f70806cc041de8
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2022-02-01 02:58:16 +0200

    My own left-handed patch for Bibata Cursor
    See also https://github.com/ful1e5/Bibata_Cursor/issues/112

--- user-specific/mouse-cursor/default.nix
+++ user-specific/mouse-cursor/default.nix
@@ -3,12 +3,74 @@
 { pkgs, ... }:
 let
   inherit (import ../../constants.nix) wenzelUserName;
+
+  left-handed-bibata-cursors = pkgs.bibata-cursors.overrideAttrs (srcAttrs: {
+    nativeBuildInputs = srcAttrs.nativeBuildInputs ++ [
+      pkgs.findutils
+      pkgs.gnugrep
+      pkgs.perl
+      pkgs.imagemagick
+    ];
+
+    patches = [
+      ./flip-x-hot-points-for-left-handed-cursors.patch
+    ];
+
+    buildPhase = ''
+      mkdir bitmaps
+      unzip $bitmaps -d bitmaps
+      rm -rf themes
+
+      (
+        set -uo pipefail || exit
+
+        images_to_flip=$(
+          cat builder/src/constants.py \
+            | perl -nE 'if (/^horizontally_flipped/) {$x=1;next}; print if $x' \
+            | perl -nE '$x=1 if /^]/; print unless $x' \
+            | sed -e 's/#.*//' -e 's/[," ]//g' \
+            | grep -v '^$'
+        )
+
+        static_images_to_flip=$(grep '\.png$' <<< "$images_to_flip")
+        animated_images_to_flip=$(grep -v '\.png$' <<< "$images_to_flip")
+        readarray -t static_images_to_flip <<< "$static_images_to_flip"
+        readarray -t animated_images_to_flip <<< "$animated_images_to_flip"
+
+        cd bitmaps
+        themes=$(ls)
+        readarray -t themes <<< "$themes"
+
+        for theme in "''${themes[@]}"; do
+          (
+            cd -- "$theme"
+
+            >&2 printf 'Horizontally flipping static images for "%s" theme...\n' "$theme"
+            for image in "''${static_images_to_flip[@]}"; do
+              (set -x && convert -flop -- "$image" "$image")
+            done
+
+            >&2 printf 'Horizontally flipping animated frames for "%s" theme...\n' "$theme"
+            for file_prefix in "''${animated_images_to_flip[@]}"; do
+              files=$(find -name "''${file_prefix}*.png")
+              readarray -t files <<< "$files"
+              for image in "''${files[@]}"; do
+                (set -x && convert -flop -- "$image" "$image")
+              done
+            done
+          )
+        done
+      )
+
+      cd builder && make build_unix
+    '';
+  });
 in
 {
   home-manager.users.${wenzelUserName} = {
     xsession.pointerCursor = {
       name = "Bibata-Original-Ice";
-      package = pkgs.bibata-cursors;
+      package = left-handed-bibata-cursors;
       defaultCursor = "left_ptr";
       size = 24;
     };
