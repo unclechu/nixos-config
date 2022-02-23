d40d0955b6b3aea6005d16e4ac5bd056eaa75b49
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2022-02-23 05:32:35 +0200

    Update Psi+ pin

--- apps/psi-plus.nix
+++ apps/psi-plus.nix
@@ -3,13 +3,13 @@
 { fetchFromGitHub, psi-plus, libsForQt5 }:
 
 (psi-plus.override { enablePsiMedia = true; }).overrideAttrs (srcAttrs: rec {
-  version = "1.5.1600";
+  version = "1.5.1605";
 
   src = fetchFromGitHub {
     owner = "psi-plus";
     repo = "psi-plus-snapshots";
     rev = version;
-    sha256 = "1f1vzyhkhkjm0hjhqpspg8l6ksikf8an77b2ywd4cq99a49b3501";
+    sha256 = "1x7pysjjhvchqs5y5apm9wfvm95zczhqvgi67zai69a7glvw3bjj";
   };
 
   buildInputs = srcAttrs.buildInputs ++ [
