16dc0d4f374fa4c1d99a97683eb31f87f54023dc
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2022-07-12 00:51:21 +0300

    Alacritty: Add ability to customize config locally

--- apps/alacritty/default.nix
+++ apps/alacritty/default.nix
@@ -4,8 +4,12 @@ let sources = import ../../nix/sources.nix; in
 { callPackage
 , writeTextFile
 , lib
-, dash
+
+, bash
 , alacritty
+, yaml2json
+, json2yaml
+, jq
 
 # Overridable dependencies
 , __nix-utils ? callPackage sources.nix-utils {}
@@ -16,30 +20,73 @@ let sources = import ../../nix/sources.nix; in
 , __lightColorsConfigSrc ? ./colors-light.yml # A derivation or a path
 }:
 let
-  inherit (__nix-utils) esc wrapExecutable nameOfModuleWrapDir shellCheckers;
-  name = nameOfModuleWrapDir (builtins.unsafeGetAttrPos "a" { a = 0; }).file;
-  dash-exe = "${dash}/bin/dash";
-  alacritty-exe = "${alacritty}/bin/alacritty";
+  inherit (__nix-utils) shellCheckers;
+  esc = lib.escapeShellArg;
+  name = "alacritty";
+
+  e = {
+    bash = "${bash}/bin/bash";
+    alacritty = "${alacritty}/bin/alacritty";
+    yaml2json = "${yaml2json}/bin/yaml2json";
+    json2yaml = "${json2yaml}/bin/json2yaml";
+    jq = "${jq}/bin/jq";
+  };
+
+  es = builtins.mapAttrs (lib.const lib.escapeShellArg) e;
+
+  overridableConfig = ''
+    ${es.yaml2json} \
+      | ${es.jq} -s --argjson x "$local_cfg" --argjson y "$local_color_cfg" '.[0] * $x * $y' \
+      | ${es.json2yaml}
+  '';
 
   basicCheckPhase = ''
-    ${shellCheckers.fileIsExecutable dash-exe}
-    ${shellCheckers.fileIsExecutable alacritty-exe}
+    ${builtins.concatStringsSep "\n" (map shellCheckers.fileIsExecutable (builtins.attrValues e))}
     ${shellCheckers.fileIsReadable "${__configSrc}"}
     ${shellCheckers.fileIsReadable "${__darkColorsConfigSrc}"}
     ${shellCheckers.fileIsReadable "${__lightColorsConfigSrc}"}
   '';
 
-  buildExecutable = name: config:
-    assert builtins.isString name;
-    wrapExecutable alacritty-exe {
-      inherit name;
-      args = ["--config-file=${config}"];
+  buildExecutable = name: color: config: writeTextFile {
+    name = assert builtins.isString name; name;
+    executable = true;
+    destination = "/bin/${name}";
 
-      checkPhase = ''
-        ${basicCheckPhase}
-        ${shellCheckers.fileIsReadable config}
-      '';
-    } // { inherit config; };
+    text = assert builtins.elem color [ "dark" "light" ]; ''
+      #! ${e.bash}
+      set -o errexit || exit
+      set -o nounset
+      set -o pipefail
+
+      LOCAL_CFG_FILE=$HOME/.wenzels-alacritty-local-config.yml
+      LOCAL_COLOR_CFG_FILE=$HOME/.wenzels-alacritty-${esc color}-local-config.yml
+
+      local_cfg='{}' # JSON
+      local_color_cfg='{}' # JSON
+      if [[ -f $LOCAL_CFG_FILE ]]; then
+        local_cfg=$(${es.yaml2json} < "$LOCAL_CFG_FILE")
+      fi
+      if [[ -f $LOCAL_COLOR_CFG_FILE ]]; then
+        local_color_cfg=$(${es.yaml2json} < "$LOCAL_COLOR_CFG_FILE")
+      fi
+
+      exec ${es.alacritty} --config-file=<((${overridableConfig}) < ${esc config}) "$@"
+    '';
+
+    checkPhase = ''
+      ${basicCheckPhase}
+      ${shellCheckers.fileIsReadable config}
+      (
+        set -o nounset
+        set -o pipefail
+        local_cfg='{"foo":{"abc":10}}'
+        local_color_cfg='{"foo":{"def":20}}'
+        x=$((${overridableConfig}) <<< $'foo:\n bar:\n  baz: 123' | ${es.yaml2json} | ${es.jq} -cS)
+        f () { [[ $x == '{"foo":{"abc":10,"bar":{"baz":123},"def":20}}' ]]; }
+        f || (set -o xtrace; f)
+      )
+    '';
+  } // { inherit config; };
 
   buildConfig = name: colorsConfigFile: optionalFullConfig: font:
     assert builtins.isString name;
@@ -96,14 +143,17 @@ let
     assert ! isNull font -> builtins.isString font;
     let
       default =
-        buildExecutable name (buildConfig darkName __darkColorsConfigSrc defaultConfig font);
+        buildExecutable name "dark"
+          (buildConfig darkName __darkColorsConfigSrc defaultConfig font);
     in
     default // {
       inherit default;
       dark =
-        buildExecutable darkName (buildConfig darkName __darkColorsConfigSrc darkConfig font);
+        buildExecutable darkName "dark"
+          (buildConfig darkName __darkColorsConfigSrc darkConfig font);
       light =
-        buildExecutable lightName (buildConfig lightName __lightColorsConfigSrc lightConfig font);
+        buildExecutable lightName "light"
+          (buildConfig lightName __lightColorsConfigSrc lightConfig font);
     };
 in
 customize {} // {
