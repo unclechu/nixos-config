9527d31d17b9330ab55ac440e4c94fb20e3acdb9
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2023-04-29 22:00:30 +0300

    Update Ardour pin 7.3 → 7.4

--- apps/ardour.nix
+++ apps/ardour.nix
@@ -5,15 +5,15 @@ let sources = import ../nix/sources.nix; in
 
 { lib, fetchgit, ardour }:
 
-# Using newer Ardour (7.3 instead of 7.1)
+# Using newer Ardour (7.4 instead of 7.1)
 ardour.overrideAttrs (old: rec {
   # “postInstall” contains links to 7 major version of Ardour
-  version = assert lib.versions.major old.version == "7"; "7.3";
+  version = assert lib.versions.major old.version == "7"; "7.4";
 
   src = fetchgit {
     url = "git://git.ardour.org/ardour/ardour.git";
     rev = version;
-    hash = "sha256-fDZGmKQ6qgENkq8NY/J67Jym+IXoOYs8DT4xyPXLcC4=";
+    hash = "sha256-CUGhJi3ji0F6v41Y08sQvo7oKITOJ96ojdJL+FyCxmw=";
   };
 
   postPatch = builtins.replaceStrings [old.version] [version] old.postPatch;
