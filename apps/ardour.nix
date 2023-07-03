8e0f714cfb8622158604710b18e966eb339ae61a
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2023-07-03 03:49:04 +0300

    Update Ardour 7.4 → 7.5

--- apps/ardour.nix
+++ apps/ardour.nix
@@ -5,15 +5,15 @@ let sources = import ../nix/sources.nix; in
 
 { lib, fetchgit, ardour }:
 
-# Using newer Ardour (7.4 instead of 7.1)
+# Using newer Ardour (7.5 instead of 7.3)
 ardour.overrideAttrs (old: rec {
   # “postInstall” contains links to 7 major version of Ardour
-  version = assert lib.versions.major old.version == "7"; "7.4";
+  version = assert lib.versions.major old.version == "7"; "7.5";
 
   src = fetchgit {
     url = "git://git.ardour.org/ardour/ardour.git";
     rev = version;
-    hash = "sha256-CUGhJi3ji0F6v41Y08sQvo7oKITOJ96ojdJL+FyCxmw=";
+    hash = "sha256-cmYt6fGYuuVs6YhAXaO9AG6TrYLDVUaE1/iC67rt76I=";
   };
 
   postPatch = builtins.replaceStrings [old.version] [version] old.postPatch;
