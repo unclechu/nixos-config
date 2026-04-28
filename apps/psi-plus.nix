e8137bd85d9819263a43290e7ff666ee60127807
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2026-04-28 04:14:15 +0300

    System update 25.11.8972 → 25.11.9840
    - nixos channel 25.11.8972.4e92bbcdb030 → 25.11.9840.a4bf06618f0b
    - nixos-unstable 26.05pre975402.68d8aa3d661f → 26.05pre985613.0726a0ecb6d4
    - neovimrc pin 5900bd5e → dcbd3bec (remove catppuccin-mocha colorscheme
      that fails to build with Neovim from unstable nixpkgs)
    - Remove Psi+ 1.5.2073 pin (using newer 1.5.2115 from stable nixpkgs)

--- apps/psi-plus.nix
+++ apps/psi-plus.nix
@@ -1,17 +1,12 @@
 # Author: Viacheslav Lotsmanov
 # License: MIT https://raw.githubusercontent.com/unclechu/nixos-config/master/LICENSE
 
-let sources = import ../nix/sources.nix; in
-
 { psi-plus
 , libsForQt5
 , libomemo-c
 }:
 
 (psi-plus.override { enablePsiMedia = true; }).overrideAttrs (srcAttrs: rec {
-  version = sources.psi-plus.branch;
-  src = sources.psi-plus;
-
   buildInputs = srcAttrs.buildInputs ++ [
     libsForQt5.qtimageformats
     libsForQt5.qtkeychain
