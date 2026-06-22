8edf2b3406c86433ed35ed1953b299ebb1527814
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2026-06-22 08:29:31 +0300

    xmonad-dev.sh: Set HOME as working directory before starting XMonad

--- gui/xmonad/xmonad-dev.sh
+++ gui/xmonad/xmonad-dev.sh
@@ -3,6 +3,8 @@ set -o errexit || exit; set -o errtrace; set -o nounset; set -o pipefail
 SCRIPT_DIR=$(dirname -- "${BASH_SOURCE[0]}"); cd -- "$SCRIPT_DIR"
 PWD=$(pwd)
 POLYBAR_RUN_SCRIPT=$(realpath -- "$PWD/../polybar/run-polybar.sh")
+XMONAD_EXE=$PWD/xmonad
 export POLYBAR_RUN_SCRIPT
 export XMONAD_DEV=1
-exec -a xmonad ./xmonad "$@"
+cd -- "$HOME"
+exec -a xmonad "$XMONAD_EXE" "$@"
