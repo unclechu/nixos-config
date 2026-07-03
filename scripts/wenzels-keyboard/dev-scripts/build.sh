fc1dd47681bb575ee79a24226a8ed7b0df0a300f
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2026-07-03 20:24:53 +0300

    wenzels-keyboard: Move dev scripts to dev-scripts dir

--- scripts/wenzels-keyboard/dev-scripts/build.sh
+++ scripts/wenzels-keyboard/dev-scripts/build.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env dash
 set -o errexit || exit; set -o nounset; set -o pipefail
-SCRIPT_DIR=$(dirname -- "$0"); CDPATH='' cd -- "$SCRIPT_DIR"
+SCRIPT_DIR=$(dirname -- "$0"); CDPATH='' cd -- "$SCRIPT_DIR/.." # Project root
 set -o xtrace
 exec nim compile --mm:atomicArc -o:wenzels-keyboard ./wenzels_keyboard.nim "$@"
--- scripts/wenzels-keyboard/dev-scripts/lint.sh
+++ scripts/wenzels-keyboard/dev-scripts/lint.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env dash
 set -o errexit || exit; set -o nounset; set -o pipefail
-SCRIPT_DIR=$(dirname -- "$0"); CDPATH='' cd -- "$SCRIPT_DIR"
+SCRIPT_DIR=$(dirname -- "$0"); CDPATH='' cd -- "$SCRIPT_DIR/.." # Project root
 set -o xtrace
 exec nim check \
 	--colors:off --styleCheck:error --hintAsError:XDeclaredButNotUsed:on \
