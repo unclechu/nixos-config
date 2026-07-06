75f6d4ccdd13938104aab8b659e1bc2a61cbd423
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2026-07-06 05:04:30 +0300

    wenzels-keyboard dev-run.sh Read values for options from constants.toml

--- scripts/wenzels-keyboard/dev-scripts/dev-run.sh
+++ scripts/wenzels-keyboard/dev-scripts/dev-run.sh
@@ -10,10 +10,18 @@ SCRIPT_DIR=$(dirname -- "$0"); CDPATH='' cd -- "$SCRIPT_DIR/.." # Project root
 ARGS=$(<./config.toml clunky-toml-json-converter toml2json | jq -re '.nim."build-arguments" | .[]')
 readarray -t ARGS_LIST <<< "$ARGS"
 
+CONSTANTS_JSON=$(<../../constants.toml clunky-toml-json-converter toml2json)
+KEY_REPEAT_DELAY=$(<<<"$CONSTANTS_JSON" jq -er .keyRepeat.delay)
+KEY_REPEAT_INTERVAL=$(<<<"$CONSTANTS_JSON" jq -er .keyRepeat.interval)
+XKB_LAYOUT=$(<<<"$CONSTANTS_JSON" jq -er .xkb.layout)
+XKB_OPTIONS=$(<<<"$CONSTANTS_JSON" jq -er .xkb.options)
+
+OPTIONS_ARGS=(
+	--key-repeated-delay "$KEY_REPEAT_DELAY"
+	--key-repeated-interval "$KEY_REPEAT_INTERVAL"
+	--xkb-layout "$XKB_LAYOUT"
+	--xkb-options "$XKB_OPTIONS"
+)
+
 set -o xtrace
-exec nim compile --run "${ARGS_LIST[@]}" \
-	--key-repeated-delay=170 \
-	--key-repeated-interval=30 \
-	--xkb-layout='us,ru,fi' \
-	--xkb-options='eurosign:e,grp:shifts_toggle' \
-	"$@"
+exec nim compile --run "${ARGS_LIST[@]}" "${OPTIONS_ARGS[@]}" "$@"
